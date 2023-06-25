d1aff8e8bbc068eab08d1d3e42fb4ea0a33557f1
Pollutedlake <psh950827@naver.com>
2023-06-26 03:57:20 +0900

    ImageManager

--- Portfolio2/Portfolio2/ImageManager.cpp
+++ Portfolio2/Portfolio2/ImageManager.cpp
@@ -1,2 +1,41 @@
 #include "Stdafx.h"
 #include "ImageManager.h"
+
+HRESULT ImageManager::init(void)
+{
+	return S_OK;
+}
+
+void ImageManager::release(void)
+{
+}
+
+GImage* ImageManager::addImage(string key, const char* fileName, int width, int height, bool isTrans, COLORREF transColor)
+{
+	GImage* image = findImage(key);
+	if (image != nullptr)
+	{
+		return image;
+	}
+
+	image = new GImage;
+	if (image->init(fileName, width, height, isTrans, transColor))
+	{
+		SAFE_DELETE(image);
+		return nullptr;
+	}
+
+	_imageMap.insert(make_pair(key, image));
+
+	return image;
+}
+
+GImage* ImageManager::findImage(string key)
+{
+	
+	return nullptr;
+}
+
+void ImageManager::deleteImage(string key)
+{
+}
--- Portfolio2/Portfolio2/ImageManager.h
+++ Portfolio2/Portfolio2/ImageManager.h
@@ -5,12 +5,16 @@
 class ImageManager : public SingletonBase <ImageManager>
 {
 private:
-	map<string, GImage*> imageMap;
+	map<string, GImage*> _imageMap;		// image들을 관리하는 맵
 	
 public:
 	HRESULT init(void);
 	void release(void);
 
-	GImage* addImage(string key, int width, int height);s
+	GImage* addImage(string key, const char* fileName, int width, int height, bool isTrans = false, COLORREF transColor = RGB(0, 0, 0));
+
+	GImage* findImage(string key);
+	void deleteImage(string key);
+	//void renderImage(string key, HDC hdc, int desX, int desY, )
 };
 
